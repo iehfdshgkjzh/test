5ec78209f6ec20d15cfd3e447b8b0036e380a6a7
iehfdshgkjzh <1335017540@qq.com>
2019-11-19 16:32:39 +0800

    mission3

--- Classjs.js
+++ Classjs.js
@@ -5,17 +5,3 @@ $("#88").click(function(){
         });
     });
 });
-
-
-/*
-function page(list){
-    document.getElementById('monday').outerHTML = '<td>&nbsp;</td><td colspan="2">' + list[0].one + '</td><td colspan="2">' + list[1].two + '</td><td colspan="2">' + list[2].three + '</td><td colspan="2">' + list[3].four + '</td>';
-}
-*/
-
-
-//lst = [{"one":"综合英语（一）东九A212"}, {"two":"思想道德修养与法律基础 东九C401"}, {"three":"计算机程序设计基础（python) 东十二312"}, {"four":"微积分（二） 东九C403"}];
-
-
-
-//var mon = JSON.parse('{"one":"综合英语（一）东九A212", "two":"思想道德修养与法律基础 东九C401", "three":"计算机程序设计基础（python) 东十二312", "four":"微积分（二） 东九C403"}');
