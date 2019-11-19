4cf06ce8de391086f6d7e0ed7653185932c3d541
iehfdshgkjzh <1335017540@qq.com>
2019-11-20 00:12:32 +0800

    mission3

--- classjs.js
+++ classjs.js
@@ -5,25 +5,67 @@ lst4 = ["#thu1", "#thu2", "#thu3", "#thu4"];
 lst5 = ["#fri1", "#fri2", "#fri3", "#fri4"];
 
 $(document).ready(function(){
-    $("button").click(function(){
+    $("#1").click(function(){
         $.getJSON("classjo.json", function(date){
             for (i = 0; i < 4; i++){
-                $(lst1[i]).html(date.mon[i].name);
+                $(lst1[i]).html(date.yaoxue1.mon[i].name);
             };
             for (i = 0; i < 4; i++){
-                $(lst2[i]).html(date.tue[i].name);
+                $(lst2[i]).html(date.yaoxue1.tue[i].name);
             };
             for (i = 0; i < 4; i++){
-                $(lst3[i]).html(date.wed[i].name);
+                $(lst3[i]).html(date.yaoxue1.wed[i].name);
             };
             for (i = 0; i < 4; i++){
-                $(lst4[i]).html(date.thu[i].name);
+                $(lst4[i]).html(date.yaoxue1.thu[i].name);
             };
             for (i = 0; i < 4; i++){
-                $(lst5[i]).html(date.fri[i].name);
+                $(lst5[i]).html(date.yaoxue1.fri[i].name);
             };
         });
     });
 });
 
+$(document).ready(function(){
+    $("#2").click(function(){
+        $.getJSON("classjo.json", function(date){
+            for (i = 0; i < 4; i++){
+                $(lst1[i]).html(date.yaoji.mon[i].name);
+            };
+            for (i = 0; i < 4; i++){
+                $(lst2[i]).html(date.yaoji.tue[i].name);
+            };
+            for (i = 0; i < 4; i++){
+                $(lst3[i]).html(date.yaoji.wed[i].name);
+            };
+            for (i = 0; i < 4; i++){
+                $(lst4[i]).html(date.yaoji.thu[i].name);
+            };
+            for (i = 0; i < 4; i++){
+                $(lst5[i]).html(date.yaoji.fri[i].name);
+            };
+        });
+    });
+});
 
+$(document).ready(function(){
+    $("#3").click(function(){
+        $.getJSON("classjo.json", function(date){
+            for (i = 0; i < 4; i++){
+                $(lst1[i]).html(date.yaoshi.mon[i].name);
+            };
+            for (i = 0; i < 4; i++){
+                $(lst2[i]).html(date.yaoshi.tue[i].name);
+            };
+            for (i = 0; i < 4; i++){
+                $(lst3[i]).html(date.yaoshi.wed[i].name);
+            };
+            for (i = 0; i < 4; i++){
+                $(lst4[i]).html(date.yaoshi.thu[i].name);
+            };
+            for (i = 0; i < 4; i++){
+                $(lst5[i]).html(date.yaoshi.fri[i].name);
+            };
+        });
+    });
+});
