a1fbbbc904ef190108aead2b5cfc6022e2bed96b
iehfdshgkjzh <1335017540@qq.com>
2019-11-20 18:47:39 +0800

    mission3

--- classjs.js
+++ classjs.js
@@ -1,70 +1,40 @@
-lst1 = ["#mon1", "#mon2", "#mon3", "#mon4"];
-lst2 = ["#tue1", "#tue2", "#tue3", "#tue4"];
-lst3 = ["#wed1", "#wed2", "#wed3", "#wed4"];
-lst4 = ["#thu1", "#thu2", "#thu3", "#thu4"];
-lst5 = ["#fri1", "#fri2", "#fri3", "#fri4"];
+var day = new Array();
+day[0] = ["#mon1", "#mon2", "#mon3", "#mon4"];
+day[1] = ["#tue1", "#tue2", "#tue3", "#tue4"];
+day[2] = ["#wed1", "#wed2", "#wed3", "#wed4"];
+day[3] = ["#thu1", "#thu2", "#thu3", "#thu4"];
+day[4] = ["#fri1", "#fri2", "#fri3", "#fri4"];
+
 
 $(document).ready(function(){
     $("#1").click(function(){
         $.getJSON("classjo.json", function(date){
-            for (i = 0; i < 4; i++){
-                $(lst1[i]).html(date.yaoxue1.mon[i].name);
-            };
-            for (i = 0; i < 4; i++){
-                $(lst2[i]).html(date.yaoxue1.tue[i].name);
-            };
-            for (i = 0; i < 4; i++){
-                $(lst3[i]).html(date.yaoxue1.wed[i].name);
-            };
-            for (i = 0; i < 4; i++){
-                $(lst4[i]).html(date.yaoxue1.thu[i].name);
-            };
-            for (i = 0; i < 4; i++){
-                $(lst5[i]).html(date.yaoxue1.fri[i].name);
+            for (t = 0; t < 5; t++){
+                for (i = 0; i < 4; i++){
+                    $(day[t][i]).html(date.yaoxue1[t].day[i].name);
+                };
             };
         });
     });
 });
-
 $(document).ready(function(){
     $("#2").click(function(){
         $.getJSON("classjo.json", function(date){
-            for (i = 0; i < 4; i++){
-                $(lst1[i]).html(date.yaoji.mon[i].name);
-            };
-            for (i = 0; i < 4; i++){
-                $(lst2[i]).html(date.yaoji.tue[i].name);
-            };
-            for (i = 0; i < 4; i++){
-                $(lst3[i]).html(date.yaoji.wed[i].name);
-            };
-            for (i = 0; i < 4; i++){
-                $(lst4[i]).html(date.yaoji.thu[i].name);
-            };
-            for (i = 0; i < 4; i++){
-                $(lst5[i]).html(date.yaoji.fri[i].name);
+            for (t = 0; t < 5; t++){
+                for (i = 0; i < 4; i++){
+                    $(day[t][i]).html(date.yaoji[t].day[i].name);
+                };
             };
         });
     });
 });
-
 $(document).ready(function(){
     $("#3").click(function(){
         $.getJSON("classjo.json", function(date){
-            for (i = 0; i < 4; i++){
-                $(lst1[i]).html(date.yaoshi.mon[i].name);
-            };
-            for (i = 0; i < 4; i++){
-                $(lst2[i]).html(date.yaoshi.tue[i].name);
-            };
-            for (i = 0; i < 4; i++){
-                $(lst3[i]).html(date.yaoshi.wed[i].name);
-            };
-            for (i = 0; i < 4; i++){
-                $(lst4[i]).html(date.yaoshi.thu[i].name);
-            };
-            for (i = 0; i < 4; i++){
-                $(lst5[i]).html(date.yaoshi.fri[i].name);
+            for (t = 0; t < 5; t++){
+                for (i = 0; i < 4; i++){
+                    $(day[t][i]).html(date.yaoshi[t].day[i].name);
+                };
             };
         });
     });
