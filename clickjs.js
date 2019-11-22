80aa2e062a15a4f94e947b929c4c8a89d67b6bcf
iehfdshgkjzh <1335017540@qq.com>
2019-11-23 00:35:25 +0800

    trans

--- clickjs.js
+++ clickjs.js
@@ -1,38 +1,24 @@
+lst = ["pic1.png", "pic2.png", "pic3.png", "pic4.png", "pic5.png"];
 
 document.onclick = function click(){
+    var mun = Math.random();
+    mun = parseInt(mun*10)%5;
+    for (i=0; i<5; i++){
+        if (mun == i){
+            pict = lst[i];
+        };
+    };
     var node = document.getElementsByTagName("body")[0];
     var di = document.createElement("div");
     var e = event || window.event;
     di.style.position = "absolute";
-    var x = e.clientX;
-    var y = e.clientY
-    di.style.left = x;
-    di.style.top = y;
+    di.style.left = e.clientX+'px';
+    di.style.top = e.clientY+'px';
     var im = document.createElement("img");
-    alert(x+","+y);
-    im.src = "pic1.jpg";
+    im.src = pict;
     im.id = "picture";
     di.appendChild(im);
     node.appendChild(di);
-    setTimeout(function(){di.style.display='none';}, 7000);
-}
+    setTimeout(function(){node.removeChild(di)}, 6998);
+};
 
-
-
-
-/*
-$("body").click(function(){
-    var x = $(this).offset().left;
-    var y = $(this).offset().top;
-    new node = document.getElementById("99");
-    new di = document.createElement("div");
-    di.style.position = "absolute";
-    di.style.left = x;
-    di.style.top = y;
-    new im = document.createElement("img");
-    im.src = "pic1.jpg";
-    im.id = "clickimg";
-    di.appendChild(im);
-    node.appendChild(di);
-});
-*/
